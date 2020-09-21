1931734f4bb092f3952f653d6c0e8363826c8549
Evandro Murilo <evandromurilob@hotmail.com>
2020-09-21 13:56:58 -0400

    working on chapter 3 (numbers)

--- chapter_3/chapter_3.sql
+++ chapter_3/chapter_3.sql
@@ -10,3 +10,30 @@ VALUES ('abc', 'abc', 'abc'),
 
 COPY char_data_types TO '/home/murilo/Projects/practical_sql/mine/chapter_3/typetest.txt'
 WITH (FORMAT CSV, HEADER, DELIMITER '|');
+
+CREATE TABLE number_data_types(
+    numeric_column numeric(20, 5),
+    real_column real,
+    double_column double precision
+);
+
+INSERT INTO number_data_types
+VALUES (.7, .7, .7),
+       (2.13579, 2.13579, 2.13579),
+       (2.1357987654, 2.1357987654, 2.1357987654);
+      
+SELECT * FROM number_data_types;
+
+/*
+ numeric_column | real_column | double_column
+----------------+-------------+---------------
+        0.70000 |         0.7 |           0.7
+        2.13579 |     2.13579 |       2.13579
+        2.13580 |      2.1358 |  2.1357987654
+(3 rows)
+*/
+
+SELECT numeric_column * 10000000 AS "Fixed",
+       real_column    * 10000000 AS "Float"
+FROM number_data_types
+WHERE numeric_column = .7;
