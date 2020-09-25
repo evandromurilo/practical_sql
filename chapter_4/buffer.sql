37665e19f755da856c71097aa38c99d7c477d7aa
Evandro Murilo <evandromurilob@hotmail.com>
2020-09-24 22:49:28 -0400

    export training

--- chapter_4/buffer.sql
+++ chapter_4/buffer.sql
@@ -1,9 +1,1 @@
-CREATE TEMPORARY TABLE supervisor_salaries_temp (LIKE supervisor_salaries);
-
-\copy supervisor_salaries_temp (town, supervisor, salary) FROM './supervisor_salaries.csv' WITH (FORMAT CSV, HEADER);
-
-INSERT INTO supervisor_salaries (town, county, supervisor, salary)
-SELECT town, 'Some County', supervisor, salary
-FROM supervisor_salaries_temp;
-
-DROP TABLE supervisor_salaries_temp;
+\copy (SELECT geo_name, state_us_abbreviation FROM us_counties_2010 WHERE geo_name ILIKE '%mill%') TO './us_counties_mill_export.csv' WITH (FORMAT CSV, HEADER);
